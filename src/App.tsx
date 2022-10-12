062a9409ad55f20975ea06d82307e4ea17831486
okaken2022 <okaken.address@gmail.com>
2022-10-13 07:50:39 +0900

    TypeScriptのデータ型

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name = "hello";
+
+let username: string ="Hello";
+let dummyNum: number = 2;
+let bool: boolean = true;
+
+// 配列での型定義
+let array1 = [true, false, true];
+let array2 = [0, 1, "hello"];
+
+// オブジェクトの型定義
+interface NAME {
+  first: string;
+  last: string | null;
+}
+let nameObj: NAME = {first:"Yamada", last:"Taro"};
+
+// 関数の型定義
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
