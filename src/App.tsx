3b061effd684775b6689a60b3d80d38bb7efdc47
okaken2022 <okaken.address@gmail.com>
2022-10-14 22:32:02 +0900

    Literal Types, typeof

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,43 @@ const func1 = (x: number, y: number): number => {
   return x + y;
 }
 
+//Intersection Types
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy",
+}
+
+// Literal Types
+
+let company: "Facebook" | "Google" | "Amazon"
+company = "Amazon"
+// company = "Apple" エラーになる
+
+let memory: 256 | 512;
+memory = 256;
+
+//typeof (msgの型をmsg2継承)
+let msg: string = "Hi";
+let msg2: typeof msg;
+    // msg2 = 2; エラーになる
+let animal = { cat: "small cat"};
+let newAnimal: typeof animal = { cat: "big cat" };
+
+
 function App() {
   return (
     <div className="App">
