9f44f13e0662a4b4370a7c494fcb3460b2cd1319
okaken2022 <okaken.address@gmail.com>
2022-10-17 04:31:15 +0900

    usestate

--- src/App.tsx
+++ src/App.tsx
@@ -165,7 +165,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <TestComponent text="hello from App />
+        // <TestComponent text="hello from App />
       </header>
     </div>
   );
